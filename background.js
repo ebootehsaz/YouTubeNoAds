02f2fb310c082ae3e136d506ed63e5a5274fc104
Ethan Bootehsaz <ethandb2024@gmail.com>
2023-10-26 21:46:46 -0400

    transitioned to manifest v3

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
+self.addEventListener('install', function(event) {
+  // Service worker installation
+  console.log('Service Worker installed');
+});
+
 chrome.webNavigation.onBeforeNavigate.addListener(
-    function(details) {
-      var url = new URL(details.url);
-      if (url.hostname === "www.youtube.com" && url.pathname === "/watch") {
-        var newUrl = "https://yewtu.be" + url.pathname + url.search;
-        chrome.tabs.update(details.tabId, {url: newUrl});
-      }
-    },
-    {url: [{urlPrefix: "https://www.youtube.com/watch"}]}
-  );
-  
+  function(details) {
+    let url = new URL(details.url);
+    if (url.hostname === "www.youtube.com" && url.pathname === "/watch") {
+      let newUrl = "https://yewtu.be" + url.pathname + url.search;
+      chrome.tabs.update(details.tabId, {url: newUrl});
+    }
+  },
+  {url: [{urlPrefix: "https://www.youtube.com/watch"}]}
+);
